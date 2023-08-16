87f66db8604f71b8bfd7945c58aefda30757486f
CaptaiN785 <thakurmukeshkumar785@gmail.com>
2023-08-17 01:08:28 +0530

    done some changes

--- back-end/config/storage.js
+++ back-end/config/storage.js
@@ -1,10 +1,18 @@
 const { initializeApp } = require("firebase/app");
-
+require("dotenv").config()
 var firebaseApp;
 const connectStorage = () => {
-    try{   
-        const firebaseConfig = require("../firebaseStorageKey.json")
-        firebaseApp = initializeApp(firebaseConfig);
+    try{
+        const config = {
+            apiKey: process.env.apiKey,
+            authDomain:process.env.authDomain,
+            projectId:process.env.projectId,
+            storageBucket:process.env.storageBucket,
+            messagingSenderId:process.env.messagingSenderId,
+            appId:process.env.appId,
+            measurementId:process.env.measurementId
+        }
+        firebaseApp = initializeApp(config);
         console.log("Storage connected!")
     }catch(err){
         console.log("Error while connecting storage", err);
